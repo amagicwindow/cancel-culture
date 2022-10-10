b5d42bac3c588ea86d2929a68831790552b60a55
magicwindow <fairlybased@protonmail.com>
2022-10-10 05:12:30 +0000

    some changes

--- get-tweets.sh
+++ get-tweets.sh
@@ -6,9 +6,8 @@ while getopts "u:" o; do
     case "${o}" in
         u)
             username=${OPTARG}
-mkdir output/${username} 
-echo "Generating a report of deleted tweets for ${username}"
-target/release/twcc deleted-tweets --report ${username} > output/${username}/deleted_tweets.md
-echo "Finished. Please see `output/${username}/deleted_tweets.md`"
+            echo "Generating a report of deleted tweets for ${username}"
+            target/release/twcc deleted-tweets --report ${username} > output/${username}_deleted_tweets.md
+            echo "Finished. Please see `output/${username}/deleted_tweets.md`"
     esac
 done
